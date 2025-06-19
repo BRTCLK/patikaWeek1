da9496e19b53a7ca034934d064e5c8e46cd1083c
BRTCLK <lberatcelik0@gmail.com>
2025-06-19 23:17:11 +0300

    Proje dosyası ekle.

---
+++ pratikIlkUygulamam/Program.cs
@@ -0,0 +1,3 @@
+﻿Console.WriteLine("Berat Çelik");
+
+Console.WriteLine("Patika'nın uzman kadrosu ve tecrübeli eğitmenleri sayesinde yazılım dünyasına hızlı bir şekilde giriş yapabileceğim için bu bootcampteyim!");
