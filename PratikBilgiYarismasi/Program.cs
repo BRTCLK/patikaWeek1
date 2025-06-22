e06f472560b099684412199f7c8caa02bbe1fe7b
BRTCLK <lberatcelik0@gmail.com>
2025-06-22 20:48:37 +0300

    bilgi yarışması pratik eklendi.

---
+++ PratikBilgiYarismasi/Program.cs
@@ -0,0 +1,72 @@
+﻿int correctAnswers = 0; // Doğru cevap sayısını tutar
+
+Console.WriteLine("1 -> Kızınca tüküren hayvan hangisidir ?");
+Console.WriteLine("a) Lama   b) Deve");
+Console.Write("Cevabınız: ");
+string answer1 = Console.ReadLine().ToLower();
+
+if (answer1 == "a")
+{
+    correctAnswers++;
+    Console.WriteLine("Doğru cevap!");
+}
+else
+{
+    Console.WriteLine("Yanlış cevap!");
+}
+
+Console.WriteLine();
+
+Console.WriteLine("2 -> Dünya'ya en yakın gezegen hangisidir ?");
+Console.WriteLine("a) Venüs   b) Mars");
+Console.Write("Cevabınız: ");
+string answer2 = Console.ReadLine().ToLower();
+
+if (answer2 == "a")
+{
+    correctAnswers++;
+    Console.WriteLine("Doğru cevap!");
+}
+else
+{
+    Console.WriteLine("Yanlış cevap!");
+}
+
+Console.WriteLine();
+
+// İlk iki sorudan sonra büyük ödül şansı bitti mi kontrol edelim
+if (correctAnswers < 1)
+{
+    // İlk 2 sorudan hiç doğru yapmadıysa, 3. soruyu sormaya gerek yok
+    Console.WriteLine("Büyük ödül kazanma şansınız kalmadı!");
+}
+else
+{
+    // Büyük ödül şansı devam ediyor, 3. soruyu soralım
+    Console.WriteLine("3 -> 5 * 2 + 8 / 2 - 2 işleminin sonucu kaçtır ?");
+    Console.WriteLine("a) 7   b) 12");
+    Console.Write("Cevabınız: ");
+    string answer3 = Console.ReadLine().ToLower();
+
+    if (answer3 == "b")
+    {
+        correctAnswers++;
+        Console.WriteLine("Doğru cevap!");
+    }
+    else
+    {
+        Console.WriteLine("Yanlış cevap!");
+    }
+}
+
+Console.WriteLine();
+
+// Sonuç
+if (correctAnswers >= 2)
+{
+    Console.WriteLine("Tebrikler! 1 Milyon TL'lik büyük ödülü kazandınız!");
+}
+else
+{
+    Console.WriteLine("Maalesef büyük ödülü kazanamadınız. Daha şanslı bir dahaki sefere!");
+}
--- PratikSwitchCase/Program.cs
+++ PratikSwitchCase/Program.cs
@@ -1,42 +1,4 @@
-﻿//Console.WriteLine("Rüya Manavına Hoş Geldiniz!");
-//Console.WriteLine("Elma = 2 TL");
-//Console.WriteLine("Armut = 3 TL");
-//Console.WriteLine("Çilek = 2 TL");
-//Console.WriteLine("Muz = 3 TL");
-//Console.WriteLine("Diğer bütün meyveler = 4 TL");
-//Console.WriteLine();
-
-//Console.Write("Hangi meyveyi satın almak istiyorsunuz? (Elma/Armut/Çilek/Muz/Diğer): ");
-//string fruit = Console.ReadLine().ToLower(); // Büyük-küçük harf farkını kaldırdık
-//int price;
-
-//switch (fruit)
-//{
-//    case "apple":
-//        price = 2;
-//        break;
-
-//    case "pear":
-//        price = 3;
-//        break;
-
-//    case "strawberry":
-//        price = 2;
-//        break;
-
-//    case "banana":
-//        price = 3;
-//        break;
-
-//    default:
-//        price = 4;
-//        break;
-//}
-
-//Console.WriteLine($"Seçilen meyvenin fiyatı: {price} TL");
-
-
-Console.WriteLine("Rüya Manavına Hoş Geldiniz!");
+﻿Console.WriteLine("Rüya Manavına Hoş Geldiniz!");
 Console.WriteLine("Elma = 2 TL");
 Console.WriteLine("Armut = 3 TL");
 Console.WriteLine("Çilek = 2 TL");
@@ -44,34 +6,60 @@ Console.WriteLine("Muz = 3 TL");
 Console.WriteLine("Diğer bütün meyveler = 4 TL");
 Console.WriteLine();
 
-Console.Write("Hangi meyveyi satın almak istersiniz? (Elma/Armut/Çilek/Muz/Diğer): ");
-string fruit = Console.ReadLine().ToLower();
-
+Console.Write("Hangi meyveyi satın almak istiyorsunuz? (Elma/Armut/Çilek/Muz/Diğer): ");
+string fruit = Console.ReadLine().ToLower(); // Büyük-küçük harf farkını kaldırdık
 int price;
 
-if (fruit == "elma")
+switch (fruit)
 {
-    price = 2;
-}
-else if (fruit == "armut")
-{
-    price = 3;
-}
-else if (fruit == "çilek")
-{
-    price = 2;
-}
-else if (fruit == "muz")
-{
-    price = 3;
+    case "apple":
+        price = 2;
+        break;
+
+    case "pear":
+        price = 3;
+        break;
+
+    case "strawberry":
+        price = 2;
+        break;
+
+    case "banana":
+        price = 3;
+        break;
+
+    default:
+        price = 4;
+        break;
 }
 
-else
-{
-    price = 4; // Bilinmeyen tüm meyveler için diğer fiyatı
-}
+Console.WriteLine($"Seçilen meyvenin fiyatı: {price} TL");
 
-Console.WriteLine($"Seçtiğiniz meyvenin fiyatı: {price} TL");
+
+
+//if (fruit == "elma")
+//{
+//    price = 2;
+//}
+//else if (fruit == "armut")
+//{
+//    price = 3;
+//}
+//else if (fruit == "çilek")
+//{
+//    price = 2;
+//}
+//else if (fruit == "muz")
+//{
+//    price = 3;
+//}
+
+//else
+//{
+//    price = 4; // Bilinmeyen tüm meyveler için diğer fiyatı
+//}
+
+//Console.WriteLine($"Seçtiğiniz meyvenin fiyatı: {price} TL");
 
 
 
