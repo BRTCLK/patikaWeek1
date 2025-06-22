ad41d804f6ead1431b2f91274a4413b78be30ccc
BRTCLK <lberatcelik0@gmail.com>
2025-06-22 14:06:02 +0300

    yeni pratik uygulamalr eklendi

---
+++ PratikDegiskenlerUygulama/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.Text;
+
+Console.WriteLine("Lütfen aşağıdaki bilgileri giriniz: ");
+
+Console.Write("T.C. Kimlik Numarası: ");
+string id = Console.ReadLine();
+
+Console.Write("Adı: ");
+string name = Console.ReadLine();
+
+Console.Write("Soyadı: ");
+string lastname = Console.ReadLine();
+
+Console.Write("Telefon Numrası: ");
+string phoneNum = Console.ReadLine();
+// Telefon numarası string veri tipi ile saklanmıştır çünkü başında sıfır olabilir ve matematiksel işlem yapılmaz.
+
+Console.Write("Yaş: ");
+int year = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("İlk Aldığı Ürün Fiyatı: ");
+double firstProduct = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("İkinci Aldığı Ürün Fiyatı: ");
+double secondProduct = Convert.ToDouble(Console.ReadLine());
+// Son aldığı ürün fiyatı double veri tipi ile saklanmıştır çünkü fiyatlar küsuratlı olabilir.
+
+Console.WriteLine("-----------------------------------------------");
+
+// Kullanıcı kaydı oluşturuldu mesajı
+Console.WriteLine($"{id} Tc numaralı {name} {lastname} isimli kişi için kayıt oluşturulmuştur.");
+Console.WriteLine($"{phoneNum} telefon numarasına bildirim mesajı gönderilmiştir.");
+
+// Toplam harcama ve patika puan hesaplama
+double totalSpending = firstProduct + secondProduct;
+double patikaPoint = totalSpending * 0.10;
+
+Console.WriteLine($"{totalSpending} toplam harcama karşılığı kazanılan %10 patika puan miktarı -> {patikaPoint:F2} TL'dir.");
---
+++ pratikIfElse/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.Write("Lütfen bir tam sayı giriniz: ");
+
+int num = Convert.ToInt32(Console.ReadLine());
+// 10 ile karşılaştırma
+if (num > 10)
+{
+    Console.WriteLine("Girilen sayı 10'dan büyüktür.");
+}
+
+else if (num == 10)
+{
+    Console.WriteLine("Girilen sayı 10'a eşittir.");
+}
+
+else
+{
+    Console.WriteLine("Girilen sayı 10'dan küçüktür.");
+}
+// Çift / Tek kontrolü
+if (num % 2 == 0)
+{
+    Console.WriteLine( "Girilen sayı çifttir.");
+}
+else
+{
+    Console.WriteLine("Girilen sayı tekttir.");
+}
